f8cbb12a8d926d916eb81b383ff58f9d82ca3f2e
Dewayne Johnson <mdj@kandykoi.com>
2020-08-31 08:31:57 -0400

    instagram and newsletter update

--- src/sections/instagram.js
+++ src/sections/instagram.js
@@ -1,10 +1,35 @@
 import React from "react";
 
+import insta_img_1 from "../assets/images/Social_1.jpg";
+import insta_img_2 from "../assets/images/Social_2.jpg";
+import insta_img_3 from "../assets/images/Social_3.jpg";
+import insta_img_4 from "../assets/images/Social_4.jpg";
+import insta_img_5 from "../assets/images/Social_5.jpg";
+
 export default function SectionInsta() {
 	return (
 		<div className="row">
-			<div className="container-fluid instagram">
-				<div className="wrapper"></div>
+			<div className="container instagram">
+				<h2 className="instagram-title color-black text-center">
+					Follow us on Instagram!
+				</h2>
+				<div className="instagram-wrapper">
+					<div className="instagram-inner">
+						<img src={insta_img_1} width="100%" height="auto" alt="Instagram" />
+					</div>
+					<div className="instagram-inner">
+						<img src={insta_img_2} width="100%" height="auto" alt="Instagram" />
+					</div>
+					<div className="instagram-inner">
+						<img src={insta_img_3} width="100%" height="auto" alt="Instagram" />
+					</div>
+					<div className="instagram-inner">
+						<img src={insta_img_4} width="100%" height="auto" alt="Instagram" />
+					</div>
+					<div className="instagram-inner">
+						<img src={insta_img_5} width="100%" height="auto" alt="Instagram" />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/sections/newsletter.js
+++ src/sections/newsletter.js
@@ -1,10 +1,35 @@
 import React from "react";
 
-export default function SectionNewsletter() {
+export default function SectionInsta() {
 	return (
-		<div className="row">
-			<div className="container-fluid newsletter">
-				<div className="wrapper"></div>
+		<div className="row bg-main">
+			<div className="container newsletter">
+				<h2 className="newsletter-title mb-3 color-white text-center">
+					Sign up & Stay Connected
+				</h2>
+				<div className="wrapper">
+					<div className="newsletter-text color-white">
+						<p>
+							Sign up for the newsletter and get 20% off! Gain access to
+							exclusive offers and be the first to know when new stuff drops!
+						</p>
+					</div>
+					<div className="newsletter-form">
+						<input
+							type="email"
+							name="email"
+							placeholder="Your Email Address"
+							className="newsletter-email"
+						/>
+						<button
+							type="submit"
+							name="submit"
+							className="newsletter-submit button color-white bg-main"
+						>
+							Subscribe
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
